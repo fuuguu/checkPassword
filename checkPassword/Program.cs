4d7b74f41e0b675f8213427feb4b19c8139041a7
desired <desired@DESKTOP-BNNTGOI>
2023-01-23 09:46:40 +0200

    Добавьте файлы проекта.

---
+++ checkPassword/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int triesCount = 5;
+        string password = "123456";
+        string userInput;
+
+        for (int i=0; i<triesCount; i++)
+        {
+            Console.Write("Введите пароль: ");
+            userInput= Console.ReadLine();
+            if(userInput==password)
+            {
+                Console.WriteLine("Добро пожаловать");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Введен неверный пароль.");
+                    Console.WriteLine($"У вас осталось попыток: {triesCount-(i+1)}");
+            }
+        }
+    }
+}
